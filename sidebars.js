b950ad4c78233a4dce2370fa1523b823c27b93db
Michael.Han <michael.han@sickkids.ca>
2020-12-21 13:06:46 -0500

    Init project setup for deploy on github pages

---
+++ sidebars.js
@@ -0,0 +1,22 @@
+/**
+ * Copyright (c) 2017-present, Facebook, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+module.exports = {
+  someSidebar: {
+    Docusaurus: ['doc1', 'doc2', 'doc3'],
+    Features: ['mdx'],
+    // May_Challenge_Week3: ['day19','leetcode234', 'day29','day30'],
+    // June_Challenge_Week1: ['6day1'],
+    // July_Challenge: ['7day1'],
+    // Lai_Class: ['class5'],
+    CC189_TreeAndGraph: ['p250'],
+    // Tree_Graph: ['bfs2_1'],
+    BLind_Cutated_75: ['leetcode1', 'leetcode3', 'leetcode5']
+    // June_Challenge_Week1: ['6day1', '6day2','6day3', '6day4','6day5', '6day6', '6day7'],
+    // June_Challenge_Week2: ['6day8', '6day9','6day10', '6day11','6day12', '6day13', '6day14']
+  }
+};
